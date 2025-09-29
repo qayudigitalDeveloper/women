1fd67af368cd014b5cff5eda3e1b0c6829a0cb0d
theprogrammerashi <96290297+theprogrammerashi@users.noreply.github.com>
2025-09-29 14:23:41 +0530

    added all files

--- frontend/womeninkuwait-app/src/App.jsx
+++ frontend/womeninkuwait-app/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import AboutUs from "./pages/Aboutus";
@@ -15,6 +15,7 @@ import Contactus from "./pages/Contactus";
 export default function App() {
   return (
     <Routes>
+      <Route path="/index.html" element={<Navigate to="/" />} />
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
